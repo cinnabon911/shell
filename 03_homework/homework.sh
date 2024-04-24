ba840edb40bf70b2c6c3d4e778156779eb8143b7
Angel Yang <angel10132433@gmail.com>
2024-04-24 15:24:23 -0400

    homework submission

--- 03_homework/homework.sh
+++ 03_homework/homework.sh
@@ -11,9 +11,13 @@ mkdir dir1 dir2 dir3 dir4 dir5
 # 2. How would you verify the creation of all 5 directories?
 ls 
 # 3. In each directory, how would you create 5 .txt files and write "I love data" into each within the directories?
-
+for dir in */; do
+    for i in {1..5}; do
+        echo "I love data" >> "$dir/file$i.txt"
+    done
+done 
 # 4. How would you verify the presence of all 5 files?
-
+ls -R
 # 5. How would you append to one of the existing files " and machine learning!"?
 echo " and machine learning" >> dir1/file1.txt
 # 6. How would you verify that the text was indeed appended to the existing file?
